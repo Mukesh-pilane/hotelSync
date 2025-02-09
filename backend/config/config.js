8952b1b77d4b6e440f27909adb74f6185a8b6b2f
Mukesh Pilane <mukeshpilane@gmail.com>
2025-02-10 01:07:48 +0530

    added port in prod env

--- backend/config/config.js
+++ backend/config/config.js
@@ -22,6 +22,7 @@ module.exports = {
     "password": process.env.PG_PASSWORD,
     "database": process.env.PG_DATABASE,
     "host": process.env.PG_HOST,
+    "port": process.env.PG_PORT,
     "dialect": process.env.PG_DIALECT,
     "logging": false
   }
