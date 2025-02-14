ea57c5f4bd0086e864dce958d5ef09398e1a26f0
Mukesh Pilane <mukeshpilane@gmail.com>
2025-02-14 23:07:36 +0530

    change git config to ignore case

---
+++ frontend/src/pages/Hotel/Hotel.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useMemo, useEffect } from 'react'
+import Table from '../../components/shared/Table/Table'
+import { Button,  Stack } from '@mantine/core'
+import ReUsableHeader from '../../components/shared/Header/ReUsableHeader'
+import { useDisclosure } from '@mantine/hooks'
+import CustomModal from '../../components/shared/Modal/Modal'
+import HotelForm from './HotelForm'
+import { useGetHotelQuery } from '../../store/server/queries/hotelQuery'
+
+
+
+
+const Hotel = () => {
+  const [search, setSearch] = useState("")
+  const [opened, { open, close }] = useDisclosure(false);
+  const { data:hotelsData, isLoading, isError, error } = useGetHotelQuery({ page: 1, limit: 10 }); 
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name', // Maps to the full name
+        header: 'Name',
+      },
+      {
+        accessorKey: 'address', // Maps to the address
+        header: 'Address',
+      },
+    ],
+    []
+  );
+  
+  
+  return (
+    <>
+      <Stack>
+        <ReUsableHeader
+          search={search}
+          setSearch={setSearch}
+          Button={
+            <Button variant="default" onClick={open}>
+              Add Hotel
+            </Button>
+          }
+        />
+        <Table columns={columns} isLoading={hotelsData===undefined} data={hotelsData?.data || []} />
+      </Stack>
+      <CustomModal title="Hotel" opened={opened} close={close}>
+        <HotelForm data={{}} close={close} />
+      </CustomModal>
+    </>
+  )
+}
+
+export default Hotel
---
+++ frontend/src/pages/Hotel/HotelForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useForm } from '@mantine/form';
+import { Button, SimpleGrid, Group, NumberInput, TextInput, Textarea } from '@mantine/core';
+import { useAddHotelMutation } from '../../store/server/queries/hotelQuery';
+
+const initialValues = {
+    name: '',
+    address:'',
+    baseTokenPoints:''
+};
+
+const HotelForm = ({ data, close }) => {
+    const { mutate: addHotelMutation, isError, error } = useAddHotelMutation();
+
+    const modifiedData = data?.id ? data : initialValues;
+    const form = useForm({
+        mode: 'uncontrolled',
+        initialValues: modifiedData,
+    });
+
+
+    const handleSubmit = async (values) => {
+        addHotelMutation({ ...values })
+        close()
+    };
+
+    return (
+        <form
+            onSubmit={form.onSubmit(handleSubmit)}
+            style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+            <TextInput
+                withAsterisk
+                label="Name"
+                placeholder="uncle chinese"
+                key={form.key('name')}
+                {...form.getInputProps('name')}
+            />
+            <Textarea
+                withAsterisk
+                label="Address"
+                placeholder="address"
+                key={form.key('address')}
+                {...form.getInputProps('address')}
+            />
+            <NumberInput
+                withAsterisk
+                label="Base Token"
+                placeholder="100"
+                key={form.key('baseTokenPoints')}
+                {...form.getInputProps('baseTokenPoints')}
+                hideControls
+            />
+            <Group justify="flex-end" mt="md">
+                <Button type="submit">Submit</Button>
+            </Group>
+        </form>
+    )
+}
+
+export default HotelForm
